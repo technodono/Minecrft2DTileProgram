7086c36cc152cf39283f918b29ec6f487fc42600
technodono <22761234+technodono@users.noreply.github.com>
2025-07-20 23:45:44 +1000

    add saf-t blocks

--- TextureSplicer.py
+++ TextureSplicer.py
@@ -1,31 +1,7 @@
 import os
 from PIL import Image
-cube_model_blocks = [
-    "stone", "dirt", "grass_block", "oak_planks", "spruce_planks", "birch_planks", "jungle_planks",
-    "acacia_planks", "dark_oak_planks", "mangrove_planks", "bamboo_planks", "cherry_planks",
-    "crimson_planks", "warped_planks", "sand", "red_sand", "gravel", "clay", "snow_block",
-    "ice", "packed_ice", "blue_ice", "coal_block", "iron_block", "gold_block", "diamond_block",
-    "emerald_block", "netherite_block", "copper_block", "waxed_copper_block", "waxed_exposed_copper",
-    "waxed_weathered_copper", "waxed_oxidized_copper", "cut_copper", "waxed_cut_copper",
-    "redstone_block", "lapis_block", "amethyst_block", "calcite", "tuff", "dripstone_block",
-    "deepslate", "polished_deepslate", "cobbled_deepslate", "blackstone", "polished_blackstone",
-    "chiseled_polished_blackstone", "gilded_blackstone", "basalt", "smooth_basalt", "end_stone",
-    "purpur_block", "purpur_pillar", "netherrack", "nether_bricks", "red_nether_bricks",
-    "chiseled_nether_bricks", "magma_block", "soul_sand", "soul_soil", "glowstone", "shroomlight",
-    "ancient_debris", "crying_obsidian", "obsidian", "prismarine", "dark_prismarine", "sea_lantern",
-    "bricks", "stone_bricks", "mossy_stone_bricks", "cracked_stone_bricks", "chiseled_stone_bricks",
-    "deepslate_bricks", "cracked_deepslate_bricks", "deepslate_tiles", "cracked_deepslate_tiles",
-    "mud_bricks", "packed_mud", "mud", "terracotta", "white_terracotta", "orange_terracotta",
-    "magenta_terracotta", "light_blue_terracotta", "yellow_terracotta", "lime_terracotta",
-    "pink_terracotta", "gray_terracotta", "light_gray_terracotta", "cyan_terracotta",
-    "purple_terracotta", "blue_terracotta", "brown_terracotta", "green_terracotta",
-    "red_terracotta", "black_terracotta", "concrete", "white_concrete", "orange_concrete",
-    "magenta_concrete", "light_blue_concrete", "yellow_concrete", "lime_concrete", "pink_concrete",
-    "gray_concrete", "light_gray_concrete", "cyan_concrete", "purple_concrete", "blue_concrete",
-    "brown_concrete", "green_concrete", "red_concrete", "black_concrete", "hay_block", "bookshelf"
-]
 ResourcePackScale = 16
-ImageOutputDir = os.path.join("resources", "assets", "tiles", "textures", "tile")
+ImageOutputDir = os.path.join("resources", "assets", "tiles", "textures", "tile","")
 os.makedirs(ImageOutputDir, exist_ok=True)
 
 TilesetName = input("Please enter the name of the tileset you would like to generate: ")
