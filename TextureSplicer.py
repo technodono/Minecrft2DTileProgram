640d2828354dc4a72259b5830af619efb9fff27b
technodono <22761234+technodono@users.noreply.github.com>
2025-07-25 15:29:10 +1000

    -add files to github

--- TextureSplicer.py
+++ TextureSplicer.py
@@ -7,7 +7,7 @@ os.makedirs(ImageOutputDir, exist_ok=True)
 TilesetName = input("Please enter the name of the tileset you would like to generate: ")
 TilesetImage = Image.open(TilesetName +".png")
 for x in range(0,TilesetImage.width,ResourcePackScale):
-    for y in range(0,TilesetImage.height,ResourcePackScale ):
+    for y in range(0,TilesetImage.height,ResourcePackScale):
 
         quadrant = (int(x/ResourcePackScale), int(y/ResourcePackScale))
         SplicedImage = TilesetImage.crop((x,y,x+ResourcePackScale,y+ResourcePackScale))
